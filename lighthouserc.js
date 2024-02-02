0f97b3176141539f2101a1eb71876f57f473fa68
Dione Ousseynou <dioneousseynou@MacBook-Pro-de-Dione.local>
2024-02-02 20:52:22 +0100

    remove commande start server

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,26 +1,12 @@
 module.exports = {
     ci: {
       collect: {
-         startServerCommand: 'npm run server',
-      //  url: ['https://web.dev/performance-scoring/']
       url: ['https://developer.chrome.com/docs/lighthouse/performance/performance-scoring?hl=fr'],
-    //  numberOfRuns: 5
+      numberOfRuns: 5
       },
-      /*
-      assert: {
-        assertions: {
-          'categories:performance': ['warn', {minScore: 1}],
-          'categories:accessibility': ['error', {minScore: 1}]
-        }
-      },
-      */
+
       upload: {
         target: 'temporary-public-storage',
       },
     },
   };
-
-  // https://web.dev/performance-scoring/
-
-  // 2x2nL0EWez7:46842367:8QwFQRAJFbxFC
-  // LHCI_GITHUB_APP_TOKEN
