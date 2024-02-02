434f7b877e9f299105ffbcf62bc95334bbcc3ccb
Dione Ousseynou <dioneousseynou@MacBook-Pro-de-Dione.local>
2024-02-02 11:05:54 +0100

    update workflow

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,8 +4,16 @@ module.exports = {
          startServerCommand: 'npm run',
       //  url: ['https://web.dev/performance-scoring/']
       url: ['https://developer.chrome.com/docs/lighthouse/performance/performance-scoring?hl=fr'],
-      numberOfRuns: 5
+    //  numberOfRuns: 5
       },
+      /*
+      assert: {
+        assertions: {
+          'categories:performance': ['warn', {minScore: 1}],
+          'categories:accessibility': ['error', {minScore: 1}]
+        }
+      },
+      */
       upload: {
         target: 'temporary-public-storage',
       },
